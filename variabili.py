5558ca219ebfa5c9bceaaa6be348aa757ec912e6
albertostizzoli <albertostizzoli@yahoo.it>
2025-07-20 12:30:25 +0200

    insert variables

---
+++ variabili.py
@@ -0,0 +1,55 @@
+# 1): Dichiarare una variabile "nome" e assegnargli il tuo nome. Mandare a schermo.
+
+nome = "Alberto"
+print(nome)
+
+# 2): Dichiarare una variabile "eta" e assegnargli la tua età. Mandare a schermo.
+
+eta = 31
+print(eta)
+
+# 3): Dichiarare una variabile "pi" e assegnargli il valore di pi greco (3,14159). Mandare a schermo
+
+pi = 3.14159
+print(pi)
+
+# 4): Creare una variabile "lunghezza" e assegnargli un valore, quindi riassegnare la variabile a 15. Mandare a schermo.
+
+lunghezza = 10
+lunghezza = 15
+print(lunghezza)
+
+# 5): Creare una variabile "nome_completo" e assegnargli una stringa 
+# contenente il tuo nome e cognome. Mandare a schermo.
+
+nome_completo = "Alberto Stizzoli"
+print(nome_completo)
+
+# 6): Creare una variabile "eta_futura" e assegnargli il valore dell'età 
+# che avrai tra 10 anni (utilizzando la variabile età già esistente). Mandare a schermo.
+
+eta = 31
+eta_futura = eta + 10
+print(eta_futura)
+
+# 7): Creare delle variabili "nome", "cognome" ed "anno_di_nascita" ed assegnarle il valore dei valori. 
+# Mandare a schermo in un unico print. Sovrascrivere tutte le variabili e rimandare a schermo una seconda volta.
+
+nome = "Alberto"
+cognome = "Stizzoli"
+anno_di_nascita = 1993
+print (nome, cognome, anno_di_nascita)
+
+nome = "Carlo"
+cognome = "Rossi"
+anno_di_nascita = 1989
+print (nome, cognome, anno_di_nascita)
+
+# 8): Creare una variabile eta_attuale e assegnargli il valore dell'età che hai attualmente, 
+# calcolandola in base all'anno corrente. Mandare a schermo eta_attuale
+
+anno_corrente = 2025
+anno_di_nascita = 1993
+eta_attuale = anno_corrente - anno_di_nascita
+print(eta_attuale)
+
